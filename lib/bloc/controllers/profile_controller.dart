98b8703191051d5262fccb3a7e38de0f0e972d38
Cristhian David Recalde <crecalde@neaternotes.com>
2025-07-29 09:05:43 -0500

    auth controller

---
+++ lib/bloc/controllers/profile_controller.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class ProfileController extends GetxController {
+  //varibles
+  final RxString name = "".obs;
+  final RxString email = "".obs;
+  final RxString phone = "".obs;
+  
+  //metodos
+  Future<void> getProfileUid() async{
+    try{
+
+      //1. obtener el uid del usuario
+      final uid = FirebaseAuth.instance.currentUser?.uid;
+      if(uid != null){
+        //2. obtener el usuario
+        final userdb = FirebaseFirestore.instance.collection("users"); 
+        final user = await userdb.doc(uid).get();
+        if(user.exists){
+          //3. obtener los datos del usuario
+          final data = user.data();
+          name.value = data?["name"] ?? "";
+          email.value = data?["email"] ?? "";
+          phone.value = data?["phone"] ?? "";
+        }
+      }else{
+        debugPrint("No hay usuario autenticado");
+      }
+      
+
+    }catch(e){
+      debugPrint(e.toString());
+    }
+  }
+  
+}
