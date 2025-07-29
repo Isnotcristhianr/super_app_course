db90af97516ce78bf21a1b39a3cdca9d5f9b4ede
Cristhian David Recalde <crecalde@neaternotes.com>
2025-07-29 08:42:46 -0500

    Agregar nueva ruta para la vista de perfil y actualizar el menú de navegación en `HomeView` para incluir un enlace al perfil. Se mejora la estructura del código en `routes.dart` y `home.dart`.

--- lib/presentation/views/home.dart
+++ lib/presentation/views/home.dart
@@ -4,26 +4,29 @@ import 'package:get/get.dart';
 //controllers
 import '../../../bloc/controllers/auth_controller.dart';
 
-
 class HomeView extends StatelessWidget {
-   HomeView({super.key});
+  HomeView({super.key});
   final AuthController authController = Get.put(AuthController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Inicio'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_outlined),
-          ),
-        ],
-      ),
+      appBar: AppBar(title: const Text('Inicio')),
       drawer: Drawer(
         child: ListView(
           children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: Text('Perfil'),
+              onTap: () {
+                Get.toNamed("/profile");
+              },
+            ),
+            //footer
+            const Divider(),
             ListTile(
               title: Text('Cerrar Sesión'),
               onTap: () {
