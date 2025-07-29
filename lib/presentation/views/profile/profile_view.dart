80d2852e02c7159811d524f1ca4990d4c28a2736
Cristhian David Recalde <crecalde@neaternotes.com>
2025-07-29 08:46:28 -0500

    Agregar un AppBar a la vista de perfil con el título 'Perfil'. Se mejora la estructura de la vista al incluir un encabezado en la pantalla de perfil.

--- lib/presentation/views/profile/profile_view.dart
+++ lib/presentation/views/profile/profile_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Perfil'),
+      ),
       body: Center(child: Text('Profile')),
     );
   }
