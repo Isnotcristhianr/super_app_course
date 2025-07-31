83fca10fc11fb319de3bf9a6d6b49d5f78662ca0
Cristhian David Recalde <crecalde@neaternotes.com>
2025-07-31 08:23:37 -0500

    Actualizar la vista de perfil para incluir campos editables para nombre, email y teléfono. Se añade un encabezado "Editar Perfil" y se crea un nuevo widget `listaPerfil` para mejorar la presentación de los datos del usuario.

--- lib/presentation/views/profile/profile_view.dart
+++ lib/presentation/views/profile/profile_view.dart
@@ -12,18 +12,64 @@ class ProfileView extends StatefulWidget {
 
 class _ProfileViewState extends State<ProfileView> {
   final ProfileController profilecontroller = Get.put(ProfileController());
-  @override 
+  @override
   void initState() {
     super.initState();
     profilecontroller.getProfileUid();
-    
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Perfil')),
-      body: Center(child: Text('Profile')),
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          Text(
+            "Editar Perfil",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          listaPerfil(title: "Nombre", icon: Icons.person),
+          FormField(
+            builder: (context) {
+              return TextField(
+                decoration: InputDecoration(
+                  labelText: profilecontroller.name.string,
+                ),
+              );
+            },
+          ),
+          SizedBox(height: 20),
+          listaPerfil(title: "Email", icon: Icons.mail),
+          FormField(
+            builder: (context) {
+              return TextField(decoration: InputDecoration(labelText: "Email"));
+            },
+          ),
+          SizedBox(height: 20),
+          ListTile(leading: Icon(Icons.phone), title: Text("Teléfono")),
+          FormField(
+            builder: (context) {
+              return TextField(
+                decoration: InputDecoration(labelText: "Teléfono"),
+              );
+            },
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class listaPerfil extends StatelessWidget {
+  final String title;
+  final IconData icon;
+
+  const listaPerfil({super.key, required this.title, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(leading: Icon(icon), title: Text(title));
   }
 }
