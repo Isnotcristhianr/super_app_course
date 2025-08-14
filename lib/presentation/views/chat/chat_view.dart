f09f700d04e253c734babeddd0d3585834f1df4a
Cristhian David Recalde <crecalde@neaternotes.com>
2025-08-14 09:12:25 -0500

    Actualizar `pubspec.lock` y `pubspec.yaml` para incluir nuevas dependencias de chat y registrar el plugin `path_provider_foundation`. Modificar `ChatView` para implementar la funcionalidad de chat utilizando `flutter_chat_core` y `flutter_chat_ui`.

--- lib/presentation/views/chat/chat_view.dart
+++ lib/presentation/views/chat/chat_view.dart
@@ -1,12 +1,47 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class ChatView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_core/flutter_chat_core.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+
+class ChatView extends StatefulWidget {
   const ChatView({super.key});
+
+  @override
+  ChatViewState createState() => ChatViewState();
+}
+
+class ChatViewState extends State<ChatView> {
+  final _chatController = InMemoryChatController();
+
+  @override
+  void dispose() {
+    _chatController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Chat')),
+      body: Chat(
+        chatController: _chatController,
+        currentUserId: 'user1',
+        onMessageSend: (text) {
+          _chatController.insertMessage(
+            TextMessage(
+              // Better to use UUID or similar for the ID - IDs must be unique
+              id: '${Random().nextInt(1000) + 1}',
+              authorId: 'user1',
+              createdAt: DateTime.now().toUtc(),
+              text: text,
+            ),
+          );
+        },
+        resolveUser: (UserID id) async {
+          return User(id: id, name: 'John Doe');
+        },
+      ),
     );
   }
 }
